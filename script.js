634bdc3141208bacee090185e9ba2eb63299c869
Vlad <vlkovalenk@gmail.com>
2024-03-05 21:11:27 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let firstNumber = prompt('Give me first Number');
+let secondNumber = prompt('Give me second Number');
+let thirdNumber = prompt('Give me third Number');
+let arithmeticMean = 0;
+const number = 3;
+
+Number(firstNumber);
+Number(secondNumber);
+Number(thirdNumber);
+
+arithmeticMean = (firstNumber + secondNumber + thirdNumber) / number;
+
+alert('Your number is: ' + arithmeticMean);
